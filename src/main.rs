b43c7d2d2eadcc87d08638529360b100003f3562
David Schmidt <64831818+Dav321@users.noreply.github.com>
2026-07-13 22:11:17 +0200

    add static HTML and CSS for view page, dynamic duration-based animation

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
+use actix_files::Files;
 use actix_web::web::{Data, Redirect};
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
 use sqlx::{Executor, SqlitePool};
 use std::env::current_dir;
+use std::fs;
+use std::path::Path;
 
 pub struct State {
     pool: SqlitePool,
-    selected: Option<usize>
+    selected: Option<usize>,
+    duration: u8,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let path = current_dir()?;
     println!("DB dir: {:?}", path);
-    let pool = SqlitePool::connect("sqlite:bauchbinde.db?mode=rwc").await.unwrap();
+    let pool = SqlitePool::connect("sqlite:bauchbinde.db?mode=rwc")
+        .await
+        .unwrap();
     pool.execute(include_str!("sql/migrate.sql")).await.unwrap();
 
     HttpServer::new(move || {
@@ -20,27 +26,43 @@ async fn main() -> std::io::Result<()> {
             .app_data(Data::new(State {
                 pool: pool.clone(),
                 selected: None,
+                duration: 10,
             }))
             .service(index)
-            .service(control)
-            .service(view)
+            .service(view_css)
+            .service(Files::new("/", "./static").prefer_utf8(true))
     })
-        .bind(("127.0.0.1", 5000))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 5000))?
+    .run()
+    .await
 }
 
 #[get("/")]
 async fn index(_: Data<State>) -> impl Responder {
-    Redirect::to("/control").permanent()
+    Redirect::to("/view.html").permanent()
 }
 
-#[get("/control")]
-async fn control(state: Data<State>) -> impl Responder {
-    HttpResponse::Ok().body("Control away!")
-}
+#[get("/css/view.css")]
+async fn view_css(state: Data<State>) -> impl Responder {
+    let duration = state.duration as f32;
 
-#[get("/view")]
-async fn view(state: Data<State>) -> impl Responder {
-    HttpResponse::Ok().body("great view!")
+    // Durations
+    const OBJ_INTRO: f32 = 0.95;
+    const TEXT_DELAY: f32 = 0.9;
+    const TEXT_INTRO: f32 = 0.55;
+    const FADE: f32 = 0.5;
+
+    let obj = OBJ_INTRO / duration;
+    let text_delay = TEXT_DELAY / duration;
+    let text = text_delay + (TEXT_INTRO / duration);
+    let fade = 1. - (FADE / duration);
+
+    let css = fs::read_to_string(Path::new("./static/css/view.css"))
+        .unwrap()
+        .replace("/*obj*/0%", &format!("{:.4}%", obj * 100.))
+        .replace("/*textd*/0%", &format!("{:.4}%", text_delay * 100.))
+        .replace("/*text*/0%", &format!("{:.4}%", text * 100.))
+        .replace("/*fade*/0%", &format!("{:.4}%", fade * 100.));
+
+    HttpResponse::Ok().body(css)
 }
